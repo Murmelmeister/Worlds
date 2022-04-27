7578585e104172ff7c7641ed965b026e200358a5
Murmelmeister <danny.siewwwert@gmail.com>
2022-04-27 11:09:21 +0200

    Initial commit

---
+++ src/main/java/de/murmelmeister/worlds/Worlds.java
@@ -0,0 +1,17 @@
+package de.murmelmeister.worlds;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Worlds extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
